d7a2162116ae4087a15a164ad89226fa703a7fab
SilentException <mitja.skuver@gmail.com>
2012-01-09 15:08:37 +0100

    Enable browsing yesterday's EPG data

--- TvEngine3/TVLibrary/TvPlugin/TvPlugin/RadioGuideBase.cs
+++ TvEngine3/TVLibrary/TvPlugin/TvPlugin/RadioGuideBase.cs
@@ -836,8 +836,8 @@ namespace TvPlugin
               {
                 DateTime dtNow = DateTime.Now;
                 cntlDay.Reset();
-                cntlDay.SetRange(0, MaxDaysInGuide - 1);
-                for (int iDay = 0; iDay < MaxDaysInGuide; iDay++)
+                cntlDay.SetRange(0, MaxDaysInGuide);
+                for (int iDay = -1; iDay < MaxDaysInGuide; iDay++)
                 {
                   DateTime dtTemp = dtNow.AddDays(iDay);
                   string day;
@@ -866,7 +866,7 @@ namespace TvPlugin
                       break;
                   }
                   day = String.Format("{0} {1}-{2}", day, dtTemp.Day, dtTemp.Month);
-                  cntlDay.AddLabel(day, iDay);
+                  cntlDay.AddLabel(day, iDay + 1);
                 }
               }
               else
@@ -920,7 +920,7 @@ namespace TvPlugin
               _viewingTime = DateTime.Now;
               _viewingTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, _viewingTime.Hour,
                                           _viewingTime.Minute, 0, 0);
-              _viewingTime = _viewingTime.AddDays(iDay);
+              _viewingTime = _viewingTime.AddDays(iDay - 1);
               _recalculateProgramOffset = true;
               Update(false);
               SetFocus();
@@ -1068,12 +1068,12 @@ namespace TvPlugin
           DateTime dateNow = DateTime.Now.Date;
           DateTime datePrev = _viewingTime.Date;
           TimeSpan ts = dateNow - datePrev;
-          if (ts.TotalDays == 1)
+          if (ts.TotalDays > 1)
           {
             _viewingTime = DateTime.Now;
           }          
 
-          if (_viewingTime.Date.Equals(DateTime.Now.Date) && _viewingTime < DateTime.Now)
+          if (/*_viewingTime.Date.Equals(DateTime.Now.Date) && */_viewingTime < DateTime.Now)
           {
             int iStartX = GetControl((int)Controls.LABEL_TIME1).XPosition;
             GUIControl guiControl = GetControl((int)Controls.LABEL_TIME1 + 1);
@@ -1450,7 +1450,7 @@ namespace TvPlugin
 
           // Find first day in TVGuide and set spincontrol position
           int iDay = CalcDays();
-          for (; iDay < 0; ++iDay)
+          for (; iDay < -1; ++iDay)
           {
             _viewingTime = _viewingTime.AddDays(1.0);
           }
@@ -1458,7 +1458,7 @@ namespace TvPlugin
           {
             _viewingTime = _viewingTime.AddDays(-1.0);
           }
-          cntlDay.Value = iDay;
+          cntlDay.Value = iDay + 1;
         }
       }
       // ichan = number of rows
@@ -2885,7 +2885,7 @@ namespace TvPlugin
         _viewingTime = _viewingTime.AddMinutes(-_timePerBlock);
         // Check new day
         int iDay = CalcDays();
-        if (iDay < 0)
+        if (iDay < -1)
         {
           _viewingTime = _viewingTime.AddMinutes(+_timePerBlock);
         }
